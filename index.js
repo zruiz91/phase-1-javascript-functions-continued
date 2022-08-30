21f42dc8a1243f962f0c8f817714ab13f100ee5c
Zach Ruiz <powersbooth10@gmail.com>
2022-08-29 21:20:18 -0500

    commit tree 4dc74cb5444965a67f13d689b168b7639e648667

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+
+}
+
+function wrapAdjective(wrap = "*") {
+    return function (par1 = "special") {
+        return `You are ${wrap}${par1}${wrap}!`
+    }
+}
